7139fc619d49b25d67dfced15fb797a59ef17fb8
GreenCoo <nosovivan263@gmail.com>
2024-02-11 22:32:45 +1000

    make list of tests

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import fastapi
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Path
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
+from typing import Annotated
 
-app = fastapi.FastAPI()
+app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="./templates")
 
 
-@app.get("/{name}")
-async def root(request: Request, name: str) -> HTMLResponse:
+@app.get("/")
+async def root(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(
-        name="index.html", context={"name": name}, request=request
+        name="index.html", context={"tests": [1, 2, 3, 4, 5]}, request=request
     )
+
+@app.get("/{test}")
+async def page_of_test(request: Request,
+                       test: Annotated[str, Path(regex=r'^[a-zA-Z0-9\_] \* $')]
+                       ) -> HTMLResponse:
+    return
