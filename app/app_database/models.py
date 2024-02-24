d0348f692d756d596ee99a30665b49de3698a25c
GreenCoo <nosovivan263@gmail.com>
2024-02-24 20:07:44 +1000

    create relationship and restructure databases

--- app/app_database/models.py
+++ app/app_database/models.py
@@ -1,17 +1,28 @@
-from sqlalchemy import Column, Integer, String, Text
-from .database import Base
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
 
 
 class Test(Base):
-    __tablename__ = "tests"
+    __tablename__ = "Test"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(Text, nullable=False)
     body = Column(Text, nullable=False)
     answers = Column(Text, nullable=False)
+    users = relationship("TestUser", backref="test_id")
 
 
-# TODO make this user class with relationship from Test
-# class User(Base):
-#     ...
+class User(Base):
+    __tablename__ = "User"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(Text, nullable=False)
+    password = Column(Text, nullable=False)
+    tests = relationship("TestUser", backref="user_id")
 
+
+class TestUser(Base):
+    __tablename__ = "TestUser"
+    id = Column(Integer, primary_key=True)
+    test_id = Column(Integer, ForeignKey("Test.id"))
+    user_id = Column(Integer, ForeignKey("User.id"))
--- app/database.py
+++ app/database.py
@@ -9,26 +9,5 @@ Base = orm.declarative_base()
 engine = create_engine("sqlite:///../db/tests.db")
 
 
-meta = MetaData()
-TableOfQuestions = Table(
-    'tests', meta,
-    Column('id', Integer(), primary_key=True, index=True),
-    Column('name', String(), nullable=False),
-    Column('body', String(), nullable=False),
-    Column('answers', String(), nullable=False)
-)
-
-# class TableOfQuestions(Base):
-#     __tablename__ = "tests"
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String)
-#     body = Column(String)
-#     answers = Column(String)
-
-
 if __name__ == "__main__":
-    with engine.connect() as conn:
-        ins = TableOfQuestions.select()
-        result = conn.execute(ins).fetchall()
-        print(result[0])
-
+    print(0)
